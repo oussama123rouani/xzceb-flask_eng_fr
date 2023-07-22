f032ca38d047c27679426dc02636076c66ebec4a
oussama123rouani <rouanioussama123@gmail.com>
2023-07-22 08:20:52 -0400

    final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,9 @@
+from deep_translator import MyMemoryTranslator
+
+def english_to_french(english_text):
+    french_text = MyMemoryTranslator(source='english', target='french').translate(english_text)
+    return french_text
+
+def french_to_english(french_text):
+    english_text = MyMemoryTranslator(source='french', target='english').translate(french_text)
+    return english_text
